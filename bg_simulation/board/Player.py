332c630944b344e9f758197dc491d9976667ef39
kdchoi-mkt <kdchoi_mkt@kaist.ac.kr>
2021-09-05 17:18:04 +0900

    Type Hinting

--- bg_simulation/board/Player.py
+++ bg_simulation/board/Player.py
@@ -1,10 +1,17 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, List, Union
+
 import random
+
+
+if TYPE_CHECKING:
+    from ..card import Card
 
 
 class Player(object):
     def __init__(
         self,
-        card_list,
+        card_list: List["Card"],
         health: int = 40,
     ):
         # Card List: Set of Card()
@@ -23,27 +30,27 @@ class Player(object):
         if self.health < 0:
             self.live = False
 
-    def trigger_lose_ds(self, opponent, targeted):
+    def trigger_lose_ds(self, opponent: Player, targeted: "Card"):
         for card in self.get_live_card_list():
             card.when_lose_ds(self, opponent, targeted)
 
-    def trigger_targeted(self, opponent, targeted):
+    def trigger_targeted(self, opponent: Player, targeted: "Card"):
         for card in self.get_live_card_list():
             card.when_targeted(self, opponent, targeted)
 
-    def trigger_dead(self, opponent, dead):
+    def trigger_dead(self, opponent: Player, dead: "Card"):
         for card in self.get_live_card_list():
             card.when_dead(self, opponent, dead)
 
-    def trigger_summon(self, targeted):
+    def trigger_summon(self, targeted: "Card"):
         for card in self.get_live_card_list():
             card.when_summon(self, targeted)
 
-    def trigger_start_of_combat(self, opponent):
+    def trigger_start_of_combat(self, opponent: Player):
         for card in self.get_live_card_list():
             card.trigger_start_of_combat(self, opponent)
 
-    def decrease_coin(self, cost):
+    def decrease_coin(self, cost: int):
         self.coin -= cost
 
     def upgrade_tier(self):
@@ -60,29 +67,39 @@ class Player(object):
     def gain_coin(self, coin):
         self.coin += coin
 
-    def get_card_num(self):
+    def get_card_num(self) -> int:
         return len(self.card_list)
 
-    def get_live_card_num(self):
+    def get_live_card_num(self) -> int:
         return len(self.get_live_card_list())
 
-    def get_taunt_live_card_num(self):
+    def get_taunt_live_card_num(self) -> int:
         return len(self.get_live_taunt_card_list())
 
-    def get_card_list(self):
+    def get_card_list(self) -> List["Card"]:
         return self.card_list
 
-    def get_live_card_list(self):
+    def get_live_card_list(self) -> List["Card"]:
         return [card for card in self.card_list if card.is_live()]
 
-    def get_live_taunt_card_list(self):
+    def get_live_taunt_card_list(self) -> List["Card"]:
         return [card for card in self.get_live_card_list() if card.is_taunt()]
 
-    def get_live_card(self, index=None):
-        if index == None:
-            return random.choice(self.get_live_card_list())
-        else:
-            return self.get_live_card_list()[index]
+    def get_card(self, index=None) -> Union["Card", bool]:
+        if self.get_card_num() > 0:
+            if index == None:
+                return random.choice(self.get_card_list())
+            else:
+                return self.get_card_list()[index]
+        return False
+
+    def get_live_card(self, index=None) -> Union["Card", bool]:
+        if self.get_live_card_num() > 0:
+            if index == None:
+                return random.choice(self.get_live_card_list())
+            else:
+                return self.get_live_card_list()[index]
+        return False
 
     def attack(self):
         return sum(card.get_tier() for card in self.get_live_card_list()) + self.tier
